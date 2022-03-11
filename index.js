79ec6bbfc91fff5c883a6795ee90cc9d1d258105
zliang530 <96207001+zliang530@users.noreply.github.com>
2022-03-10 18:10:24 -0600

    Update index.js

--- index.js
+++ index.js
@@ -42,8 +42,6 @@ app.get('/', (req, res) => {
   res.render('index.ejs')
 })
 
-app.set('port', process.env.PORT || 3000);
-
-app.listen(port, () => {
-  console.log(`app listening on port ${port}`) 
-})
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
